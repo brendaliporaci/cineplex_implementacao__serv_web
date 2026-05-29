e4bdaf22cbcf6a4949f4f393f6c8435f03bebb11
Brenda <brendaliporaci4@gmail.com>
2026-05-28 23:48:38 -0300

    Implementa endpoint de reserva de assentos

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # Cria a aplicação Flask
@@ -23,6 +23,26 @@ assentos = [
 @app.route("/api/assentos", methods=["GET"])
 def listar_assentos():
     return jsonify(assentos)
+
+# Endpoint para reservar assentos
+@app.route("/api/reservas", methods=["POST"])
+def reservar():
+    dados = request.get_json()
+    codigos = dados.get("assentos", [])
+
+    for codigo in codigos:
+        for assento in assentos:
+            if assento["codigo"] == codigo:
+                if assento["status"] == "RESERVADO":
+                    return jsonify({
+                        "erro": f"Assento {codigo} já reservado"
+                    }), 400
+
+                assento["status"] = "RESERVADO"
+
+    return jsonify({
+        "mensagem": "Reserva realizada com sucesso"
+    })
 
 # Inicia o servidor
 if __name__ == "__main__":
