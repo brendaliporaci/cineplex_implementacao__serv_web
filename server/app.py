01a6cfba2cb323e045535ad1527313f2500292f1
Tamires da Hora <thora@id.uff.br>
2026-05-31 18:09:04 -0300

    Ajuste na nomenclatura do metodo de atualização para PUT

--- server/app.py
+++ server/app.py
@@ -57,7 +57,7 @@ def listar_assentos():
     return jsonify(assentos)
 
 # Endpoint para reservar assentos
-@app.route("/api/reservas", methods=["POST"])
+@app.route("/api/reservas", methods=["PUT"])
 def reservar():
     dados = request.get_json()
     codigos = dados.get("assentos", [])
